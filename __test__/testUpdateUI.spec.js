e00649b8d815028f67cfb691eb5d785b3b103519
Hugonin <jeanhugues.doumbe@gmail.com>
2021-01-29 00:59:26 +0100

    updated the updateUI test

--- __test__/testUpdateUI.spec.js
+++ __test__/testUpdateUI.spec.js
@@ -6,22 +6,19 @@ const { JSDOM } = jsdom;
 describe("Testing the UI update functionality", () => {
     test("UI should be updated properly with results of API call", () => {
         const dom = new JSDOM(`<div id="results"></div>`);
-        const data = { Confidence: "100",Score_tag: "P", Subjectivity: "SUBJECTIVE", Irony: "NONIRONIC" };
+        const data = { confidence: "100",score_tag: "P", subjectivity: "SUBJECTIVE", irony: "NONIRONIC" };
         const element = dom.window.document.getElementById('results'); 
 
-        expect(updateUI(element, data)).resolves.toBe( `<strong>Confidence: </strong>100<br>
-        <strong>Score_tag: </strong>P<br>
-        <strong>Subjectivity: </strong>SUBJECTIVE<br>
-        <strong>Irony: </strong>NONIRONIC`);
+        updateUI(element, data);
+
+        expect(element.innerHTML).toEqual(
+            `<strong>Confidence: </strong>${data.confidence}<br>
+            <strong>Score tag: </strong>${data.score_tag}<br>
+            <strong>Subjectivity: </strong>${data.subjectivity}<br>
+            <strong>Irony: </strong>${data.irony}`
+        );
     }); 
 
-    test("the fetch fails with an error'", () => {
-        const dom = new JSDOM(`<div id="results"></div>`);
-        const data = { Confidence: "100",Score_tag: "P", Subjectivity: "SUBJECTIVE", Irony: "NONIRONIC" };
-        const element = dom.window.document.getElementById('results'); 
-        expect(updateUI(element, data)).rejects.toMatch('error');
-
-    }); 
 
 }); 
 
