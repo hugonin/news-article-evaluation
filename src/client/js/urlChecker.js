6b23fa1cc366cf5409086de03193f2c63adacbad
Hugonin <jeanhugues.doumbe@gmail.com>
2021-01-29 22:55:06 +0100

    updated urlChecker.js and index.html files

--- src/client/js/urlChecker.js
+++ src/client/js/urlChecker.js
@@ -1,7 +1,9 @@
 function checkForURL(inputURL) {
-    let regex = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
+    let res = inputURL.match(/(http(s)?:\/\/.)?(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,6}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)/g);
     
-     return (regex == null ? false : true);
+    return (res == null ? false  : true);
  }
 
+
  export { checkForURL }
+
